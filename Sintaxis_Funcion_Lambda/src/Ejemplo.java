59e46c25305e8054995a52049d832c2ea38c0fdd
carlosninacio <cigramirez@poligran.edu.co>
2026-04-20 12:07:56 -0500

    Lección 1 - Ejemplo

--- Sintaxis_Funcion_Lambda/src/Ejemplo.java
+++ Sintaxis_Funcion_Lambda/src/Ejemplo.java
@@ -1,8 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Ejemplo {
     public static void main(String[] args) {
-
+        // Ejemplo -> Función lambda para implementar metodo action performed de la interfaz action listener
+        JButton button = new JButton("Hola");
+        button.addActionListener(
+                (event) -> System.out.println("Hola")
+        );
     }
 
-
+    public interface ActionLister{
+        public void ActionPerformed(ActionEvent event);
+    }
 }
-
