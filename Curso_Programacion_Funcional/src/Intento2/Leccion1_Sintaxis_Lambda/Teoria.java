d84834fef8b370816420f10574989bfb181e9e77
carlosninacio <cigramirez@poligran.edu.co>
2026-07-15 21:34:27 -0500

    Lección 1 - Sintaxis funciones lambda

---
+++ Curso_Programacion_Funcional/src/Intento2/Leccion1_Sintaxis_Lambda/Teoria.java
@@ -0,0 +1,38 @@
+package Intento2.Leccion1_Sintaxis_Lambda;
+
+import java.sql.SQLOutput;
+
+public class Teoria {
+
+    // Sintaxis función lambda
+    // parametros -> cuerpo
+    // Representa el metodo abstracto de una interfaz funcional
+
+    // Una interfaz funcional solo tiene un metodo abstracto. Puede tener cualquier cantidad
+    // de metodos default o estaticos, pero solo uno abstracto que puede ser representado
+    // con una función lambda
+
+    // Creación de una interfaz funcional
+    public interface Ficticia {
+        public void aceptar(int valor, int valor2); // Metodo abstracto
+    }
+
+    // Crear objeto de interfaz normalmente
+
+    Ficticia f = new Ficticia() {
+        @Override
+        public void aceptar(int valor, int valor2) {
+            System.out.println("Hola mundo");
+        }
+    };
+
+    Ficticia f2 =  (valor, valor2) -> {System.out.println("Hola Mundo " + valor);};
+
+    /*
+
+    Se puede omitir el return en casos como el siguiente:
+    Ficticia f = (valor, valor2) -> valor / valor2
+
+     */
+
+}
