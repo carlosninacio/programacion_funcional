fd5cfa10e09e3e38655df7894bdc3dee0b01dca6
carlosninacio <cigramirez@poligran.edu.co>
2026-07-15 21:44:40 -0500

    Lección 1- Ejemplo

--- Curso_Programacion_Funcional/src/Intento2/Leccion1_Sintaxis_Lambda/Ejemplo.java
+++ Curso_Programacion_Funcional/src/Intento2/Leccion1_Sintaxis_Lambda/Ejemplo.java
@@ -1,4 +1,25 @@
 package Intento2.Leccion1_Sintaxis_Lambda;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Ejemplo {
+
+    JButton button = new JButton("Hola");
+
+    public Ejemplo() {
+
+        button.addActionListener(
+                (event) -> System.out.println("Botón presionado")
+        ); // Hacer esta expresión lambda es lo mismo que hacer todo lo de abajo
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("Botón presionado");
+            }
+        });
+
+    }
 }
