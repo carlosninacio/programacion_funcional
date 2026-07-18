ee469f1e6a24cabded84370d8df02f115fb5f43a
carlosninacio <cigramirez@poligran.edu.co>
2026-07-17 21:08:32 -0500

    Leccion 5 - Lambda con devolucion

---
+++ Curso_Programacion_Funcional/src/Intento2/Leccion5_Lambda_ConDevolucion/Calculadora.java
@@ -0,0 +1,5 @@
+package Intento2.Leccion5_Lambda_ConDevolucion;
+
+public interface Calculadora {
+    public int calcular(int x, int y);
+}
---
+++ Curso_Programacion_Funcional/src/Intento2/Leccion5_Lambda_ConDevolucion/LambdaTest3.java
@@ -0,0 +1,18 @@
+package Intento2.Leccion5_Lambda_ConDevolucion;
+
+public class LambdaTest3 {
+
+    public static void main(String[] args) {
+        engine((x,y) -> x+y);
+        engine((x,y) -> x-y);
+        engine((x,y) -> x*y);
+        engine((x,y) -> x/y);
+    }
+
+    private static void engine(Calculadora cal) {
+        int x = 2, y = 4;
+        int resultado = cal.calcular(x,y);
+        System.out.println(resultado);
+    }
+
+}
